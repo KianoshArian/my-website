34a035aa2e61552a153837eb536a9ae0f48494f1
Kianosh <kianosharian3@gmail.com>
2024-12-28 18:55:34 +0330

    add new honors

--- src/pages/Honors/Honors.jsx
+++ src/pages/Honors/Honors.jsx
@@ -7,6 +7,9 @@ export const Honors = () => {
       <div className='margin' />
       <ul className='list-tab'>
         <li>
+          Presented Our Paper "An Eye Tracking Study on the Effect of Short-Form Videos (SFVs) on Student Attention" at the <span className='color'>11th Internation Symposium on Telecommunications</span> - Oct. 2024
+        </li>
+        <li>
           Ranked 9th amongst 250 teams in <span className='color'>The National Front-End Olympics</span> Selection Competitions - Oct. 2024
         </li>
         <li>
